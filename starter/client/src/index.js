4599297d34c8f25f035768d1bd05c3c68238b97a
Arpithaven <arpithav69@gmail.com>
2026-01-16 11:30:57 +0530

    Update backend and frontend changes

--- starter/client/src/index.js
+++ starter/client/src/index.js
@@ -1,21 +1,23 @@
-import { Auth0Provider } from '@auth0/auth0-react'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
-import App from './App'
-import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import App from "./App";
+import "./index.css";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUTH0_API_IDENTIFIER;
+
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
     redirectUri={window.location.origin}
-    audience={`https://${domain}/api/v2/`}
-    scope="read:todo write:todo delete:todo"
+    audience={audience}
+    scope="openid profile email"
   >
     <App />
   </Auth0Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
